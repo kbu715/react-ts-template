745a839d3d9aa82d74f3da96587f1d8ccfc9aa33
kbu715 <kbu715@gmail.com>
2021-10-14 22:11:37 +0900

    🔧 Update: webpack config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import './styles.css';
+import IMAGE from './react.png';
+import LOGO from './logo.svg';
+
 export const App = () => {
   return (
     <>
+      <img src={LOGO} alt="React Logo" width="100px" />
+      <img src={IMAGE} alt="React Logo" width="200px" />
       <h1>React Typescript Webpack Starter Template</h1>
     </>
   )
